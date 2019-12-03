fe3db36e05264787fe547002f3e70b9321e74c4d
Dali <mohamedali.boudich-ext@veolia.com>
2019-12-03 19:54:16 +0100

    change site

--- src/App.js
+++ src/App.js
@@ -1,31 +1,39 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
+import { Layout, Header, Navigation, Drawer, Content } from 'react-mdl';
+import Main from './components/main';
+import { Link } from 'react-router-dom';
 
-import Header from './components/Header';
-import About from './components/About';
-import Work from './components/work';
-import Contact from './components/Contact';
-import Navbar from './components/Navbar'
+class App extends Component {
+  render() {
+    return (
+      <div className="demo-big-content">
+    <Layout>
+        <Header className="header-color" title={<Link style={{textDecoration: 'none', color: 'white'}} to="/">MyPortfolio</Link>} scroll>
+            <Navigation>
+                <Link to="/resume">Resume</Link>
+                <Link to="/aboutme">About Me</Link>
+                <Link to="/projects">Projects</Link>
+                <Link to="/contact">Contact</Link>
+            </Navigation>
+        </Header>
+        <Drawer title={<Link style={{textDecoration: 'none', color: 'black'}} to="/">MyPortfolio</Link>}>
+            <Navigation>
+              <Link to="/resume">Resume</Link>
+              <Link to="/aboutme">About Me</Link>
+              <Link to="/projects">Projects</Link>
+              <Link to="/contact">Contact</Link>
+            </Navigation>
+        </Drawer>
+        <Content>
+            <div className="page-content" />
+            <Main/>
+        </Content>
+    </Layout>
+</div>
 
-function App() {
-  return (
-    <div className="App">
-      <Navbar></Navbar>
-      <div className="App-header">
-      <Header></Header>
-      </div>
-      
-      <div className='About'>
-        <About></About>
-      </div>
-      <div className='work'>
-        <Work></Work>
-      </div>
-      <div className='contact'>
-        <Contact></Contact>
-      </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default App;
--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,31 +1,64 @@
 import React, { Component } from 'react';
-import Fade from 'react-reveal/Fade';
-import data from '../yourdata';
+import { Grid, Cell, List, ListItem, ListItemContent } from 'react-mdl';
+
 
 class Contact extends Component {
-    state = {  }
-    render() { 
-        return (  <div>
-        <h1>
-                <Fade bottom cascade> Contact.</Fade>
-        </h1>
-        <Fade bottom>
-        <div className='contact-content'>
-                <h1>
-                Let’s create your next<br></br>
-                <span className='amazing-color'>experience together</span></h1>
-                    <a href={`mailto:${data.contactEmail}`} className='email'>{data.contactEmail}</a>
-                <ul>
-                    {data.social.map((link,index)=>(
-                        <li key={index}><a target='_blank' rel="noopener noreferrer" href={link.url}>{link.name}</a></li>
-                    ))}   
-                </ul>
-        </div>
-        </Fade>
+  render() {
+    return(
+      <div className="contact-body">
+        <Grid className="contact-grid">
+          <Cell col={6}>
+            <h2>Paul Hanna</h2>
+            <img
+              src="https://cdn2.iconfinder.com/data/icons/avatar-2/512/Fred_man-512.png"
+              alt="avatar"
+              style={{height: '250px'}}
+               />
+             <p style={{ width: '75%', margin: 'auto', paddingTop: '1em'}}>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries</p>
 
-            <span className='footer'>Made With ❤ by <a href="http://chetanverma.com/">Chetan Verma</a></span>
-        </div>);
-    }
+          </Cell>
+          <Cell col={6}>
+            <h2>Contact Me</h2>
+            <hr/>
+
+            <div className="contact-list">
+              <List>
+                <ListItem>
+                  <ListItemContent style={{fontSize: '30px', fontFamily: 'Anton'}}>
+                    <i className="fa fa-phone-square" aria-hidden="true"/>
+                    (123) 456-7890
+                  </ListItemContent>
+                </ListItem>
+
+                <ListItem>
+                  <ListItemContent style={{fontSize: '30px', fontFamily: 'Anton'}}>
+                    <i className="fa fa-fax" aria-hidden="true"/>
+                    (123) 456-7890
+                  </ListItemContent>
+                </ListItem>
+
+                <ListItem>
+                  <ListItemContent style={{fontSize: '30px', fontFamily: 'Anton'}}>
+                    <i className="fa fa-envelope" aria-hidden="true"/>
+                    someone@example.com
+                  </ListItemContent>
+                </ListItem>
+
+                <ListItem>
+                  <ListItemContent style={{fontSize: '30px', fontFamily: 'Anton'}}>
+                    <i className="fa fa-skype" aria-hidden="true"/>
+                    MySkypeID
+                  </ListItemContent>
+                </ListItem>
+
+
+              </List>
+            </div>
+          </Cell>
+        </Grid>
+      </div>
+    )
+  }
 }
- 
+
 export default Contact;

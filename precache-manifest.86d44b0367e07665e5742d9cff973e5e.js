a1fda5dddaa169fc69d8a987704a96c5349479f1
mohamed ali <mohamedali.boudich-ext@veolia.com>
2020-08-12 16:47:32 +0200

    Updates

---
+++ precache-manifest.86d44b0367e07665e5742d9cff973e5e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7b2d9a80a3423b96b1ab5bbdbe8a77cb",
+    "url": "/portofolio/index.html"
+  },
+  {
+    "revision": "e503f11a93a1013e279c",
+    "url": "/portofolio/static/css/2.e4346227.chunk.css"
+  },
+  {
+    "revision": "181be6497dbb2cb46934",
+    "url": "/portofolio/static/css/main.66e52bb4.chunk.css"
+  },
+  {
+    "revision": "e503f11a93a1013e279c",
+    "url": "/portofolio/static/js/2.02967f08.chunk.js"
+  },
+  {
+    "revision": "1dd2c2f40ba60d67a05f24474f30e8d3",
+    "url": "/portofolio/static/js/2.02967f08.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "181be6497dbb2cb46934",
+    "url": "/portofolio/static/js/main.faa7b8eb.chunk.js"
+  },
+  {
+    "revision": "f50e490b50393a0a5039",
+    "url": "/portofolio/static/js/runtime-main.16074c42.js"
+  },
+  {
+    "revision": "c5e7d7c6b5e972af67fa42bfe698144c",
+    "url": "/portofolio/static/media/me.c5e7d7c6.png"
+  }
+]);

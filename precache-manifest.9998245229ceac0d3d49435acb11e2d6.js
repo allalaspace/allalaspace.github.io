fe36d9fc6148485e73a3e998514bc671f7fbe8f7
mohamed ali <mohamedali.boudich-ext@veolia.com>
2020-08-12 16:17:35 +0200

    Updates

---
+++ precache-manifest.9998245229ceac0d3d49435acb11e2d6.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b9dab34be523c3c21ec233e6b84afb30",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e503f11a93a1013e279c",
+    "url": "/static/css/2.e4346227.chunk.css"
+  },
+  {
+    "revision": "db3830869c1c78b29e66",
+    "url": "/static/css/main.66e52bb4.chunk.css"
+  },
+  {
+    "revision": "e503f11a93a1013e279c",
+    "url": "/static/js/2.02967f08.chunk.js"
+  },
+  {
+    "revision": "1dd2c2f40ba60d67a05f24474f30e8d3",
+    "url": "/static/js/2.02967f08.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "db3830869c1c78b29e66",
+    "url": "/static/js/main.97c4e3c7.chunk.js"
+  },
+  {
+    "revision": "5eeb8d0fd802ead6c0b5",
+    "url": "/static/js/runtime-main.aa4df6ff.js"
+  },
+  {
+    "revision": "c5e7d7c6b5e972af67fa42bfe698144c",
+    "url": "/static/media/me.c5e7d7c6.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portofolio/precache-manifest.86d44b0367e07665e5742d9cff973e5e.js"
+  "/precache-manifest.9998245229ceac0d3d49435acb11e2d6.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/portofolio/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
